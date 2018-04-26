eb6be6ae2f34a3fff995bb53aa985d547e1cbffc
lgabite <32849027+lgabite@users.noreply.github.com>
2018-04-26 17:31:52 -0300

    Update auth.js

--- security/auth.js
+++ security/auth.js
@@ -1,12 +1,23 @@
+var db = require('../util/pooldb.js')
+
 exports.checkAuth = function(req, res, callback){
-    if (req.get('key') == "1232"){
-        req.body.groupid = 1
-        callback(req, res, req.params.id)
-    }else{
-        res.status(401).send();
-    }
-
+    sql = "select id from companygroup where apikey = '" + req.get('key') + "'";
+    
+    db.pool.connect().then( client => {
+        client.query(sql, function(err, result){
+            if (err){
+                client.end()
+                res.status(500).send(err);
+            }else{
+                client.end()
+                if (result.rows.length == 0){
+                    res.status(401).send();
+                }else{
+                    req.body.groupid = result.rows[0].id
+                    callback(req, res, req.params.id)
+                }
+                
+            }
+        })
+    })    
 }
-
-
-
