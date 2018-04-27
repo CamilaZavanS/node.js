5a2d55d5530a294b38b5d0ce317c775c20504cbc
lgabite <32849027+lgabite@users.noreply.github.com>
2018-04-26 21:19:18 -0300

    Update siteDAO.js

--- model/siteDAO.js
+++ model/siteDAO.js
@@ -11,35 +11,35 @@ exports.siteData = function (req, res){
     db.pool.connect().then( client => {
         client.query(classSQL, function(err, result){
             if (err){
-                client.end()
+                client.release()
                 res.status(500).send(err);
             }else{
                 resultSite[0] = {classes: result.rows}
                 client.query(planSQL, function(err, result){
                     if (err){
-                        client.end()
+                        client.release()
                         res.status(500).send(err);
                     }else{
                         resultSite[1] = {plans: result.rows}
                         client.query(productSQL, function(err, result){
                             if (err){
-                                client.end()
+                                client.release()
                                 res.status(500).send(err);
                             }else{
                                 resultSite[2] = {products: result.rows}
                                 client.query(unitSQL, function(err, result){
                                     if (err){
-                                        client.end()
+                                        client.release()
                                         res.status(500).send(err);
                                     }else{
                                         resultSite[3] = {units: result.rows}
                                         client.query(groupSQL, function(err, result){
                                             if (err){
-                                                client.end()
+                                                client.release()
                                                 res.status(500).send(err);
                                             }else{
-                                                client.end()
-                                                resultSite[4] = {infos:  result.rows}
+                                                client.release()
+                                                resultSite[4] = {companyData:  result.rows}
                                                 res.status(200).send( JSON.stringify({sitedata: resultSite}));
                                             }
                                         })                                         
